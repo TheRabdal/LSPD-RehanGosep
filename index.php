81ed4ba8f058ca9a74428f1f9a5d528e354638d9
Muhammad Kautsar Rizqi Abdal <kautsarrabdal@gmail.com>
2025-11-07 04:44:44 +0700

    LSPD-RehanGosep-73514

--- index.php
+++ index.php
@@ -12,14 +12,14 @@
 <body>
     <div class="hero">
         <div class="container">
-            <h1>Los Santos Police Departement</h1>
-            <h1>Rehan Gosep</h1>
-            <h1>73514</h1>
-            <p>Rehan Gosep adalah seorang officer polisi berpangkat Rookie dengan badge number 73514, beliau mengabdi di Los Santos Police Departeman pada hari Sabtu, 25 Oktober 2025.</p>
+            <h1 class="main-title">Los Santos Police Departement</h1>
+            <h1 class="sub-title">Rehan Gosep</h1>
+            <h1 class="badge-number">73514</h1>
+            <p>Rehan Gosep adalah seorang officer polisi berpangkat Rookie dengan badge number 73514, beliau mengabdi di Los Santos Police Departeman sejak 25 Oktober 2025.</p>
             <p><a href="screens/dashboard.php" class="button">ON Duty!</a></p>
             <?php
                 date_default_timezone_set('Asia/Jakarta');
-                echo "<p>" . date("H:i:s T, l, d F Y") . "</p>";
+                echo "<p class=\"current-datetime\">" . date("H:i:s T, l, d F Y") . "</p>";
             ?>
         </div>
     </div>
